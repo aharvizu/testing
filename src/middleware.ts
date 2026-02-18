340ec6308dc3d49a262741bf6aaf4a706534245f
aharvizu <aaron.h.arvizu@gmail.com>
2026-02-17 17:43:29 -0700

    Cambios 5

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,24 @@
 import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
-export default withAuth({
-  pages: { signIn: '/login' },
-});
+export default withAuth(
+  function middleware(req) {
+    console.log('[MIDDLEWARE] Authorized user accessing:', req.nextUrl.pathname);
+    console.log('[MIDDLEWARE] Token:', JSON.stringify(req.nextauth?.token));
+    return NextResponse.next();
+  },
+  {
+    pages: { signIn: '/login' },
+    callbacks: {
+      authorized({ token, req }) {
+        console.log('[MIDDLEWARE] Checking auth for:', req.nextUrl.pathname);
+        console.log('[MIDDLEWARE] Token exists:', !!token);
+        console.log('[MIDDLEWARE] NEXTAUTH_SECRET set:', !!process.env.NEXTAUTH_SECRET);
+        return !!token;
+      },
+    },
+  },
+);
 
 export const config = {
   matcher: [
